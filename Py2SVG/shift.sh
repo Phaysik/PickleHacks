c512b5fb83fdf02788d6a7aa1bf49d562b9a68d1
Phaysik <mmoore0111@gmail.com>
2020-11-14 01:37:04 -0600

    Automate a step

--- Py2SVG/shift.sh
+++ Py2SVG/shift.sh
@@ -1,11 +1,13 @@
 main() {
-    echo "This file must be run after turning the svg into gcode"
-
     if [ "$#" -ne 1 ]; then 
-        echo "You must pass in the filename of the gcode output file"
+        echo "You must pass in the filename of the GCode output file"
         exit 0
     fi
 
+    echo "Converting the SVG to GCode"
+    python svg2gcode.py "$1"
+
+    echo "Shifting the GCode Values"
     python shift.py "$1"
 }
 
