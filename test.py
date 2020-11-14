bfa66b95068b47d9494a5a4332e57fc456f192e1
Waapp <wap5h3@mst.edu>
2020-11-14 00:35:06 -0600

    working on driver

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 from LineDraw import linedraw
+import random
+from ProcessImages import combine
+def MakeThatShitIntoLines():
+    namesFile = open('./Scraping/filenames.txt', 'r')
+    names = namesFile.readlines()
+    imageCount = len(names)
+    imageIndices = []
+    while(len(imageIndices) < ):
+        index = random.randint(0, imageCount-1)
+        if(not imageIndices.__contains__(names[index])):
+            imageIndices.append(names[index])
+    
 
-def MakeThatShitIntoLines(imagePath)
-    lines = linedraw.sketch("LineDraw/peppers.png")                                 
-    linedraw.makesvg(lines)
+            
+    #lines = linedraw.sketch(imagePath)                                 
+    #linedraw.makesvg(lines)
+    #svg2gcode.generate_gcode("output/out.svg")
+
+MakeThatShitIntoLines()
