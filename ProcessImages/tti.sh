97885b84b21fdb44ae67ac2810d3c25c911cd94d
Timm-Tomm <61246550+Timm-Tomm@users.noreply.github.com>
2020-11-14 14:08:23 -0600

    Caption functionality

--- ProcessImages/tti.sh
+++ ProcessImages/tti.sh
@@ -1,15 +1,13 @@
 main() {
-    if [ "$#" -ne 8 ]; then
-        echo "You must pass 8 arguments to this script"
-        exit 0
+    if [ -d "./Images" ]; then
+        echo "Deleting all the images in ./Images/"
+        rm -rf ./Images/*
+    else
+        echo "Creating an Images folder"
+        mkdir Images
     fi
 
-    echo "Deleting all previously created images"
-    rm -rf *.png
-
-    echo "This bash script expects the input to follow: TopLeft_Text Image_Name TopRight_Text Image_Name BottomLeft_Text Image_Name BottomRight_Text Image_Name"
-
-    python tti.py "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8"
+    python tti.py 
 }
 
 main "$@"
